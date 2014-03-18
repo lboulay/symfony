b0ce354e6466b7649de61bef2ccf7e1ff1d13d97
Laurent Boulay <boulay.l@gmail.com>
2014-03-18 17:11:03 +0100

    config

---
+++ src/Dragoon/AdminBundle/Controller/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dragoon\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class IndexController extends Controller
+{
+    public function indexAction() {
+        echo 'eee';
+    }
+}
